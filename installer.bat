2c5bc268865f9375ee36b64e7cfd4bcb8e76a0af
asyash26 <asyash26@yandex.ru>
2020-04-12 18:15:32 +0300

    add files

---
+++ installer.bat
@@ -0,0 +1,13 @@
+7z1604-x64.msi /passive /norestart /le "C:\Users\Asya\ITMO\OS\7zip-log.txt" TARGETDIR="C:\Program Files\7-Zip"
+
+paintnet_4.2.10.exe /auto TARGETDIR="C:\Program Files\Graphics\Paint" DESKTOPSHORTCUT=1
+
+inkscape-0.92.4-x64.msi /qr /norestart /lw "C:\Users\Asya\ITMO\OS\inkscape-log.txt" TARGETDIR="C:\Program Files\Graphics\Inkscape"
+
+LibreOffice_6.4.2_Win_x86.msi /passive /norestart /le "C:\Users\Asya\ITMO\OS\libreoffice-install-log.txt"
+LibreOffice_6.4.2_Win_x86_helppack_ru.msi /passive /forcerestart /le+ "C:\Users\Asya\ITMO\OS\libreoffice-install-log.txt"
+
+npp.7.8.5.Installer.exe /S
+
+jre-8u241-windows-x64.exe INSTALLCFG="C:\Users\Asya\ITMO\OS\config.txt" /L "C:\Users\Asya\ITMO\OS\jre-log.txt"
+
